0476812d9367cc2370401d157b3257e299d4068c
fernando-sebastiao <fernandosebastiao888@gmail.com>
2024-06-03 09:35:32 +0100

    updated server and so on

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,15 @@
+import cors from "@fastify/cors";
 import fastify, { FastifyInstance } from "fastify";
 import { routes } from "./routes/routes";
-import cors from "@fastify/cors"
-
 
 export const app: FastifyInstance = fastify();
 
 app.register(routes);
 
-app.register(cors, {})
+app.register(cors, {});
 
 app.get("/", async (req, rep) => {
-  rep.send({ message: "EVerthing is gonna well!" });
+  rep.send({ message: "Everthing is gonna well!" });
 });
 
 app.listen({ port: 8800 }, (err, address) => {
